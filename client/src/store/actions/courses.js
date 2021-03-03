50e803f434e6f037159b09ac642d7501adf45877
Victor Pragana <victormerigue@gmail.com>
2021-03-03 15:08:30 -0300

    Ajustando actions de cursos

--- client/src/store/actions/courses.js
+++ client/src/store/actions/courses.js
@@ -1,11 +1,11 @@
 import { actionsTypes } from "../constants/courses";
 
 const courseActions = {
-  SetEvents: payload => ({
+  SetCourses: payload => ({
     payload,
     type: actionsTypes.SET_COURSES
   }),
-  setActiveEvent: payload => ({
+  setActiveCourse: payload => ({
     payload,
     type: actionsTypes.SET_ACTIVE_COURSE
   })
